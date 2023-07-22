b21abcb1245d63335c027659c2069885c6d63b9a
Prabakaran-M <mspk028@gmail.com>
2023-07-22 17:36:50 +0530

    data added

--- src/data.js
+++ src/data.js
@@ -6,8 +6,10 @@ export const events = [
     date: "25 Jul 2023, Tue",
     time: "10:00 AM EDT",
     author: "Sarah Johnson",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ2fHxwZW9wbGVzJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2ViJTIwZGV2ZWxvcG1lbnQlMjB3b3Jrc2hvcHxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Technology", "Web Development"],
     eventMode: "Online",
     dateAndTime: "Tue, Jul 25, 2023 10:00 AM",
@@ -23,8 +25,10 @@ export const events = [
     date: "08 Aug 2023, Mon",
     time: "06:30 PM CEST",
     author: "Marco Rossi",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA1fHxwZW9wbGVzJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1556910103-1c02745aae4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Q29va2luZyUyMENsYXNzJTNBJTIwSXRhbGlhbiUyMEN1aXNpbmV8ZW58MHwwfDB8fHww&auto=format&fit=crop&w=500&q=60",
     genres: ["Food & Cooking", "Cultural"],
     eventMode: "Offline",
     dateAndTime: "Mon, Aug 8, 2023 06:30 PM",
@@ -40,8 +44,10 @@ export const events = [
     date: "20 Sep 2023, Wed",
     time: "08:00 AM WITA",
     author: "Emily Lee",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1529693662653-9d480530a697?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8WW9nYSUyMFJldHJlYXR8ZW58MHwwfDB8fHww&auto=format&fit=crop&w=500&q=60",
     genres: ["Health & Wellness", "Spiritual"],
     eventMode: "Online",
     dateAndTime: "Wed, Sep 20, 2023 08:00 AM",
@@ -57,8 +63,10 @@ export const events = [
     date: "05 Oct 2023, Thu",
     time: "03:00 PM CEST",
     author: "Claire Dubois",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVvcGxlcyUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1501084817091-a4f3d1d19e07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fFBhaW50aW5nJTIwV29ya3Nob3B8ZW58MHwwfDB8fHww&auto=format&fit=crop&w=500&q=60",
     genres: ["Art & Culture", "Creativity"],
     eventMode: "Online",
     dateAndTime: "Thu, Oct 5, 2023 03:00 PM",
@@ -74,8 +82,10 @@ export const events = [
     date: "18 Nov 2023, Sat",
     time: "09:30 AM PST",
     author: "Michael Adams",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fFN0YXJ0dXAlMjBGdW5kaW5nJTIwQ29uZmVyZW5jZXxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Business", "Entrepreneurship"],
     eventMode: "Offline",
     dateAndTime: "Sat, Nov 18, 2023 09:30 AM",
@@ -91,8 +101,10 @@ export const events = [
     date: "02 Dec 2023, Sat",
     time: "11:00 AM GMT",
     author: "Sophie Turner",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1630416920377-e43f0f9dd28d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fFBob3RvZ3JhcGh5JTIwRXhoaWJpdGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Art & Culture", "Photography"],
     eventMode: "Offline",
     dateAndTime: "Sat, Dec 2, 2023 11:00 AM",
@@ -108,8 +120,10 @@ export const events = [
     date: "14 Jan 2024, Sat",
     time: "04:00 PM CST",
     author: "Alex Ramirez",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1506157786151-b8491531f063?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TXVzaWMlMjBGZXN0aXZhbHxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Music", "Festival"],
     eventMode: "Online",
     dateAndTime: "Sat, Jan 14, 2024 04:00 PM",
@@ -125,8 +139,10 @@ export const events = [
     date: "27 Feb 2024, Mon",
     time: "07:00 AM AEDT",
     author: "Emma Wilson",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1519336555923-59661f41bb45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fEZpdG5lc3MlMjBCb290Y2FtcHxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Health & Wellness", "Fitness"],
     eventMode: "Online",
     dateAndTime: "Mon, Feb 27, 2024 07:00 AM",
@@ -142,8 +158,10 @@ export const events = [
     date: "10 Mar 2024, Sat",
     time: "03:30 PM EST",
     author: "David Chen",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1491841651911-c44c30c34548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8U2NpZW5jZSUyMEZpY3Rpb24lMjBCb29rJTIwQ2x1YnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Literature", "Science Fiction"],
     eventMode: "Online",
     dateAndTime: "Sat, Mar 10, 2024 03:30 PM",
@@ -159,8 +177,10 @@ export const events = [
     date: "22 Apr 2024, Sun",
     time: "10:00 AM SGT",
     author: "Jennifer Tan",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1517630800677-932d836ab680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fERpZ2l0YWwlMjBNYXJrZXRpbmclMjBDb25mZXJlbmNlfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Marketing", "Digital"],
     eventMode: "Offline",
     dateAndTime: "Sun, Apr 22, 2024 10:00 AM",
@@ -176,8 +196,10 @@ export const events = [
     date: "16 Jun 2023, Sat",
     time: "02:00 PM ETD",
     author: "Robert Fox",
-    authorProfile: "",
-    image: "",
+    authorProfile:
+      "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHBlb3BsZXMlMjBwcm9maWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    image:
+      "https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fFdlYiUyMERlc2lnbiUyMFdvcmtzaG9wfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     genres: ["Technology", "Movies", "Games"],
     eventMode: "Online",
     dateAndTime: "Thu, Mar 9, 2023 02:00 PM-United State Of America Time",
